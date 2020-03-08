120af2ea0b1cccb1d96d734b2d8c9d48523ca1ee
CosminBenegui <61460679+CosminBenegui@users.noreply.github.com>
2020-03-08 18:00:48 +0200

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Angela Duca on 08/03/2020.
+//  Copyright © 2020 Cosmin Benegui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
